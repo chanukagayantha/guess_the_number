3da4d7787dc1bfd41f1bd53b6a79aa31d025e15c
SehanHerath <sehanjeewantha12@gmail.com>
2024-06-05 00:52:54 +0530

    start

---
+++ script.js
@@ -0,0 +1,24 @@
+var randomNumber = Math.floor(Math.random()*100)+1;
+var attempts =0;
+
+function checkGuess(){
+    var guessInput= document.getElementById('guessInput');
+    var guess=Number(guessInput.value);
+    var resultMessage= document.getElementById('resultMessage');
+
+    attempts++;
+    if (guess===randomNumber){
+        resultMessage.textContent="Congratulations! You guessed the number in "+attempts + " attempts.";
+        resultMessage.style.color='green';
+    }else if(guess> randomNumber){
+        resultMessage.textContent='Too high! Try again.';
+        resultMessage.style.color='red';
+    }else{
+        resultMessage.textContent = 'Too low! Try again.';
+        resultMessage.style.color ='red';
+    }
+
+    guessInput.value ='';
+    guessInput.focus();
+
+}
